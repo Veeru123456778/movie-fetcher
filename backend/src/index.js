53054d97757dd16b49cee48f5f5e9b8cb64788cb
Veeru123456778 <130406704+Veeru123456778@users.noreply.github.com>
2024-05-30 12:40:01 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const Port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://movie-fetcher-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }));
